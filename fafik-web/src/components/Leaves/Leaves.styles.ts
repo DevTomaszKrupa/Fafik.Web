25aa17688178fbff81bd44405c7a5a35949e0ac0
Tomasz Krupa <etomaszkrupa@gmail.com>
2019-08-05 16:37:53 +0200

    fix

---
+++ fafik-web/src/components/Leaves/Leaves.styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export default {
+    Header: styled.div`
+        display: flex;
+        flex-direction: column;
+    `,
+    Images: styled.div`
+        display: flex;
+        justify-content: space-between;
+    `
+};
--- fafik-web/src/components/Leaves/Leaves.tsx
+++ fafik-web/src/components/Leaves/Leaves.tsx
@@ -4,31 +4,25 @@ import { LeavesHeaderLeft } from '../LeavesHeaderLeft/LeavesHeaderLeft';
 import { LeavesHeaderMenu } from '../LeavesHeaderMenu/LeavesHeaderMenu';
 import { LeavesCouple } from '../LeavesCouple/LeavesCouple';
 import { LeavesCounter } from '../LeavesCounter/LeavesCounter';
-import styled from 'styled-components';
-
-const S = {
-    Header: styled.div`
-    display: flex;
-    flex-direction: column;
-    `,
-    Images: styled.div`
-    display: flex;
-    justify-content: space-between;
-    `
-};
+import components from './Leaves.styles';
 
 type Props = {
     logoNames: string;
 };
 
-const Leaves = (props: Props) =>
-    <S.Header>
-        <S.Images>
-            <LeavesHeaderLeft />
-            <LeavesHeaderRight />
-        </S.Images>
-        <LeavesHeaderMenu logoNames={props.logoNames} />
-        <LeavesCouple />
-        <LeavesCounter />
-    </S.Header>;
+const Leaves = (props: Props) => {
+    const { Header, Images } = components;
+
+    return (
+        <Header>
+            <Images>
+                <LeavesHeaderLeft />
+                <LeavesHeaderRight />
+            </Images>
+            <LeavesHeaderMenu logoNames={props.logoNames} />
+            <LeavesCouple />
+            <LeavesCounter />
+        </Header>);
+};
+
 export { Leaves };

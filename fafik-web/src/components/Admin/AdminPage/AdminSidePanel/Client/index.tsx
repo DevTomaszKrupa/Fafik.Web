a0a198494c20dcf5103f0f9f1f5e8dff29c98dfd
Klaudia Krupa <eklaudiakrupa@gmail.com>
2019-09-19 23:13:48 +0200

    Fixed active button bug on New Post Section

--- fafik-web/src/components/Admin/AdminPage/AdminSidePanel/Client/index.tsx
+++ fafik-web/src/components/Admin/AdminPage/AdminSidePanel/Client/index.tsx
@@ -54,7 +54,7 @@ const AdminSidePanelClientComponent = (props: Props) => {
             <SidePanelPause />
             <SidePanelItem onClick={OnConfigurationClicked} > Konfiguruj </SidePanelItem>
             <SidePanelItem onClick={() => OnItemClicked(adminSidePanelPaths.blog)}
-                active={history.location.pathname === adminSidePanelPaths.blog}> Blog </SidePanelItem>
+                active={history.location.pathname === (adminSidePanelPaths.blog || adminSidePanelPaths.blogNewPost)}> Blog </SidePanelItem>
             <SidePanelItem onClick={() => OnItemClicked(adminSidePanelPaths.playlist)}
                 active={history.location.pathname === adminSidePanelPaths.playlist}> Playlista </SidePanelItem>
             <SidePanelItem onClick={() => OnItemClicked(adminSidePanelPaths.guestList)}

92bfb4a957a779009523f64aad7b27713dd48760
Klaudia Krupa <eklaudiakrupa@gmail.com>
2019-09-29 21:42:59 +0200

    Added feature video

--- fafik-web/src/client/Home/HomePage/styles.ts
+++ fafik-web/src/client/Home/HomePage/styles.ts
@@ -3,7 +3,6 @@ import YouTube from 'react-youtube';
 import { IMAGE_STORAGE } from 'application/config';
 
 export default {
-
   BannerBox: styled.div`
   background-image: url("${IMAGE_STORAGE}app/main-banner.jpg");
   padding: 5rem 0;
@@ -16,193 +15,197 @@ export default {
   `,
 
   BannerInfoBox: styled.div`
-  background-color: rgba(255,255,255,0.5);
-  padding: 3.5rem 0;
-  width: 48%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-  font-family: 'Josefin Sans';
-  letter-spacing: 0.17rem;
+    background-color: rgba(255, 255, 255, 0.5);
+    padding: 3.5rem 0;
+    width: 48%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    font-family: 'Josefin Sans';
+    letter-spacing: 0.17rem;
   `,
 
   BannerInfoText: styled.div`
-  width: 74%;
-  text-align: center;
-  font-size: 1rem;
-  line-height: 2.4rem;
-  color: #253535;
+    width: 74%;
+    text-align: center;
+    font-size: 1rem;
+    line-height: 2.4rem;
+    color: #253535;
   `,
 
   BannerInfoButton: styled.button`
-  width: 51%;
-  padding: 0.73rem 0 0.7rem 0;
-  background-color: #FDFCFD;
-  border: 0.2rem solid #253535;
-  margin-top: 1.3rem;
-  font-weight: 700;
-  font-family: 'Josefin Sans';
-  letter-spacing: 0.17rem;
-  font-size: 0.97rem;
-  color: #253535;
-  cursor: pointer;
-  display: flex;
-  justify-content: center;
+    width: 51%;
+    padding: 0.73rem 0 0.7rem 0;
+    background-color: #fdfcfd;
+    border: 0.2rem solid #253535;
+    margin-top: 1.3rem;
+    font-weight: 700;
+    font-family: 'Josefin Sans';
+    letter-spacing: 0.17rem;
+    font-size: 0.97rem;
+    color: #253535;
+    cursor: pointer;
+    display: flex;
+    justify-content: center;
 
-  &: focus{
-    outline: none;
-  }
-
-  &: hover #arrow{
-    position: relative;
-    animation: moveArrow 2s infinite;
-    animation-timing-function: ease;
-    animation-direction: alternate-reverse;
-    @keyframes moveArrow {
-      from {left: 0px;}
-      to {left: 20px;}
+    &: focus {
+      outline: none;
     }
-  }
+
+    &: hover #arrow {
+      position: relative;
+      animation: moveArrow 2s infinite;
+      animation-timing-function: ease;
+      animation-direction: alternate-reverse;
+      @keyframes moveArrow {
+        from {
+          left: 0px;
+        }
+        to {
+          left: 20px;
+        }
+      }
+    }
   `,
 
   ButtonArrow: styled.div`
-  padding: 0 0 0.1rem 0.3rem;
+    padding: 0 0 0.1rem 0.3rem;
   `,
 
   MeetUsSection: styled.div`
-  flex-grow: 1;
-  height: 32rem;
-  background-color: #FDFBFD;
-  display: flex;
+    flex-grow: 1;
+    height: 32rem;
+    background-color: #fdfbfd;
+    display: flex;
   `,
 
   MeetUsBox: styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  padding: 5.7rem 3rem 2rem 3rem;
-  align-items: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    padding: 5.7rem 3rem 2rem 3rem;
+    align-items: center;
   `,
 
   MeetUsText: styled.div`
-  text-align: center;
-  font-size: 0.75rem;
-  font-weight: 400;
-  line-height: 2.3rem;
-  font-family: 'Josefin Sans';
-  letter-spacing: 0.16rem;
-  color: #253535;
-  width: 84%;
+    text-align: center;
+    font-size: 0.75rem;
+    font-weight: 400;
+    line-height: 2.3rem;
+    font-family: 'Josefin Sans';
+    letter-spacing: 0.16rem;
+    color: #253535;
+    width: 84%;
   `,
 
   MeetUsTitle: styled.div`
-  text-align: center;
-  font-size: 1.4rem;
-  font-weight: 800;
-  font-family: 'Josefin Sans';
-  letter-spacing: 0.3rem;
-  color: #253535;
+    text-align: center;
+    font-size: 1.4rem;
+    font-weight: 800;
+    font-family: 'Josefin Sans';
+    letter-spacing: 0.3rem;
+    color: #253535;
   `,
 
   HorizontalLine: styled.div`
-  width: 8rem;
-  border-top: 0.08rem solid #233333;
-  margin: 1.3rem 0;
+    width: 8rem;
+    border-top: 0.08rem solid #233333;
+    margin: 1.3rem 0;
   `,
 
   MeetUsSignatures: styled.img`
-  width: 15rem;
-  padding-left: 28rem;
+    width: 15rem;
+    padding-left: 28rem;
   `,
 
   MeetUsImg: styled.img`
-  height: 32rem;
-  width: auto;
+    height: 32rem;
+    width: auto;
   `,
 
   WhatsThisSection: styled.div`
-  height: 32rem;
-  flex-grow: 1;
-  display: flex;
-  font-family: 'Josefin Sans';
-  border-top: 1.2rem solid #F6F6F6;
+    height: 32rem;
+    flex-grow: 1;
+    display: flex;
+    font-family: 'Josefin Sans';
+    border-top: 1.2rem solid #f6f6f6;
   `,
 
   VideoSection: styled.div`
-  width: 44%;
-  background-color: #FDFCFC;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: center;
-  padding: 5rem 0 4rem 0;
+    width: 44%;
+    background-color: #fdfcfc;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    padding: 5rem 0 4rem 0;
   `,
 
   TextSection: styled.div`
-  width: 56%;
-  background-color: #FFFEFE;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 6rem 0;
-  justify-content: space-between;
+    width: 56%;
+    background-color: #fffefe;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 6rem 0;
+    justify-content: space-between;
   `,
 
   YouTubePlayer: styled(YouTube)`
-  width: 25rem;
-  height: 17rem;
-  border-radius: 1.5rem;
+    width: 25rem;
+    height: 17rem;
+    border-radius: 1.5rem;
   `,
 
   HomePageButton: styled.button`
-  border: 0.25rem solid #F9E2DC;
-  background-color: #FFFEFE;
-  width: 23rem;
-  height: 2.6rem;
-  border-radius: 1.2rem;
-  color: #ECCCC4;
-  font-family: 'Josefin Sans';
-  text-align: center;
-  letter-spacing: 0.1rem;
-  padding-top: 0.1rem;
-  font-weight: 600;
-  padding: 0 0.8rem 0 0.8rem;
-  cursor: pointer;
+    border: 0.25rem solid #f9e2dc;
+    background-color: #fffefe;
+    width: 23rem;
+    height: 2.6rem;
+    border-radius: 1.2rem;
+    color: #ecccc4;
+    font-family: 'Josefin Sans';
+    text-align: center;
+    letter-spacing: 0.1rem;
+    padding-top: 0.1rem;
+    font-weight: 600;
+    padding: 0 0.8rem 0 0.8rem;
+    cursor: pointer;
 
-  &: hover{
-    border: 0.25rem solid #D49C8E;
-    color: #D49C8E;
-  }
+    &: hover {
+      border: 0.25rem solid #d49c8e;
+      color: #d49c8e;
+    }
   `,
 
   HomePageVideoButton: styled.button`
-  border: 0.25rem solid #BAC6B6;
-  background-color: #BAC6B6;
-  width: 23rem;
-  height: 2.6rem;
-  border-radius: 1.2rem;
-  color: #FFFFFF;
-  font-family: 'Josefin Sans';
-  text-align: center;
-  letter-spacing: 0.1rem;
-  padding-top: 0.1rem;
-  font-weight: 600;
-  padding: 0 0.8rem 0 0.8rem;
-  cursor: pointer;
+    border: 0.25rem solid #bac6b6;
+    background-color: #bac6b6;
+    width: 23rem;
+    height: 2.6rem;
+    border-radius: 1.2rem;
+    color: #ffffff;
+    font-family: 'Josefin Sans';
+    text-align: center;
+    letter-spacing: 0.1rem;
+    padding-top: 0.1rem;
+    font-weight: 600;
+    padding: 0 0.8rem 0 0.8rem;
+    cursor: pointer;
 
-  &: hover{
-    background-color: #A9B7A5;
-  }
+    &: hover {
+      background-color: #a9b7a5;
+    }
   `,
 
   ShortLogo: styled.img`
-  width: 22rem;
+    width: 22rem;
   `,
 
   WhatsThisTitle: styled.img`
-  width: 14rem;
-  margin-top: -2rem;
+    width: 14rem;
+    margin-top: -2rem;
   `,
 
   WhatsThisText: styled.div`
@@ -215,4 +218,8 @@ export default {
   margin-top: -1.5rem;
 }
   `,
+
+  FeaturesVideo: styled.div``,
+
+  FVideo: styled.video``,
 };
